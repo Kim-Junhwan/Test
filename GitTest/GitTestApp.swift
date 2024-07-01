20aecc46948bce035e903e7d6232cf39108ca742
Kim junhwan <tazanboy1@naver.com>
2024-07-01 22:17:36 +0900

    1

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by JunHwan Kim on 2024/07/01.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
